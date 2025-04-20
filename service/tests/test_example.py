e6461002c0cd47dfd9f37be206c031053b3c353a
martin.arendar <martin.arendar@gmx.ch>
2025-04-21 00:34:51 +0200

    Update files in consultation with Matteo

--- service/tests/test_example.py
+++ service/tests/test_example.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 
-def test_root():
+def test_root() -> None:
     client = TestClient(app)
     response = client.get("/")
     data = response.json()
