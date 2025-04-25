f02dab81a302fc8c81308559e1b845f1cb4f6d76
martin.arendar <martin.arendar@gmx.ch>
2025-04-25 16:43:32 +0200

    Remove attributes from PlanCreate that the request does not provide

--- service/app/schemas/plan.py
+++ service/app/schemas/plan.py
@@ -15,10 +15,6 @@ class PlanRead(SQLModel):
 
 
 class PlanCreate(SQLModel):
-    group_version_id: uuid.UUID | None = None
     name: str
     content: str
-    public_slug: str
-    is_favorite: bool | None = None
-    created_at: datetime | None = None
-    user_id: uuid.UUID
+    is_favorite: bool
