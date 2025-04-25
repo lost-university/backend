83cbea5b48097525c3810473aafb3c55e81ac15e
martin.arendar <martin.arendar@gmx.ch>
2025-04-25 16:41:58 +0200

    Temporarily remove public_slug from db because functionality is not yet implemented.

--- service/app/schemas/plan.py
+++ service/app/schemas/plan.py
@@ -9,7 +9,6 @@ class PlanRead(SQLModel):
     group_version_id: uuid.UUID
     name: str
     content: str
-    public_slug: str
     is_favorite: bool
     created_at: datetime
     user_id: uuid.UUID
