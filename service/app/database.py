5ac2001c8c0acdbcbb7f26d8a4597e1865087644
Leo Oetterli <61965711+pheonix8@users.noreply.github.com>
2025-05-12 23:29:29 +0200

    debuging ci pipeline

--- service/app/database.py
+++ service/app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 postgres_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 engine = create_engine(postgres_url, echo=True)
-
+print(postgres_url)
 
 def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
