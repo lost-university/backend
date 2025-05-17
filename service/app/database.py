35c3b32ce3ec4b76c75a6a242ebb111fc47fc48a
pheonix8 <61965711+pheonix8@users.noreply.github.com>
2025-05-17 17:45:08 +0200

    update tests

--- service/app/database.py
+++ service/app/database.py
@@ -1,13 +1,17 @@
 import os
 from collections.abc import Generator
+from typing import Any
 
 from dotenv import load_dotenv
 from sqlmodel import Session, SQLModel, create_engine
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 postgres_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 engine = create_engine(postgres_url, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session)
 
 
 def create_db_and_tables() -> None:
@@ -18,6 +22,9 @@ def drop_tables() -> None:
     SQLModel.metadata.drop_all(engine)
 
 
-def get_session() -> Generator[Session, any]:
-    with Session(engine) as session:
-        yield session
+def get_session() -> Generator[Session, Any, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- service/tests/conftest.py
+++ service/tests/conftest.py
@@ -1,1 +1,65 @@
-from .test_config import CLERKID, db_session, get_valid_auth_header, test_client
+from collections.abc import Iterator, Generator
+from typing import Annotated, Any
+
+import pytest
+from fastapi import Depends, Request
+from fastapi.testclient import TestClient
+from sqlmodel import Session, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+
+from app.database import create_db_and_tables, drop_tables, get_session
+from app.main import app
+from app.middlewares.auth_middleware import auth_dependency
+from app.services.user_service import create_user, get_user_by_clerk_id
+
+
+CLERK_ID = "test_clerk_id"
+
+
+async def override_auth_dependency(request: Request, session: Annotated[Session, Depends(get_session)]) -> Request:
+    try:
+        create_user(CLERK_ID, "test@email.com", session)
+        user = get_user_by_clerk_id("test_clerk_id", session)
+    # The bad db test case requires catching the OperationalError in the auth_dependency to correctly return the HTTP code 500
+    except OperationalError as _:
+        user = None
+    request.state.user = user
+    return request
+
+
+def override_get_session() -> Generator[Session, Any, None]:
+    engine = create_engine("sqlite:///:memory:")
+    session = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session)
+
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(autouse=True)
+def db_session() -> Iterator[None]:
+    create_db_and_tables()
+    yield
+    drop_tables()
+
+
+@pytest.fixture(autouse=True)
+def overwrite_auth_dependency() -> Iterator[None]:
+    app.dependency_overrides[auth_dependency] = override_auth_dependency
+    yield
+    app.dependency_overrides[auth_dependency] = auth_dependency
+
+
+@pytest.fixture()
+def overwrite_session_dependency() -> Iterator[None]:
+    app.dependency_overrides[get_session] = override_get_session
+    yield
+    app.dependency_overrides[get_session] = get_session
+
+
+@pytest.fixture
+def test_client() -> TestClient:
+    return TestClient(app)
