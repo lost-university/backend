db562ea13f853cd2f674654c3d8b77c15dc9896b
Roman Weber <mail@romanweber.ch>
2025-05-19 22:50:25 +0200

    run linter

--- service/app/create_tables.py
+++ service/app/create_tables.py
@@ -3,10 +3,6 @@ from database import create_db_and_tables
 # Import models so their metadata is registered with SQLModel
 from models import plan, user  # noqa: F401
 
-# Import models so their metadata is registered with SQLModel
-from models import plan, user  # noqa: F401
-
-
 def main() -> None:
     create_db_and_tables()
     print("Database tables created!")
