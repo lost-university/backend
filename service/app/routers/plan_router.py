9154b7a814804d2875c35262cd7a2ff88c0939cf
Roman Weber <roman.weber@hexagon.com>
2025-06-01 14:36:14 +0200

    error 401

--- service/app/routers/plan_router.py
+++ service/app/routers/plan_router.py
@@ -73,10 +73,6 @@ async def delete_plan(request: Request, plan_id: UUID, session: Annotated[Sessio
     except Exception as e:
         raise HTTPException(status_code=500, detail="Failed to delete plan. Please report to page-admin") from e
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a7917ce (changed router.patch to return 204)
 @router.patch("/plans/bookmark/{plan_id}", dependencies=[Depends(auth_dependency)], status_code=204)
 async def bookmark_plan(request: Request, plan_id: UUID, session: Annotated[Session, Depends(get_session)]) -> None:
     try:
